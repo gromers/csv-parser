dae10407b07196332994ff19970dc5c7ad8b1c77
gromers <geert.romers@gmail.com>
2018-03-15 01:25:57 +0100

    #2 - header should be filled
    Implementation for parsing the first line as header.

--- index.js
+++ index.js
@@ -3,5 +3,42 @@ module.exports = function (csv) {
     Header : [],
     Data   : []
   };
+
+  if (csv.length === 0)
+    return result;
+
+  var isHeader = true;
+  var array = [];
+  var value = '';
+
+  for(var i = 0; i < csv.length; i++) {
+    var currentCharacter = csv.charAt(i);
+    switch (currentCharacter) {
+      case ',':
+        array.push(value);
+        value = '';
+        break;
+      case '\n':
+        if (isHeader) {
+          result.Header = array;
+          isHeader = false;
+        }
+        else {
+          result.Data.push(array);
+        }
+        break;
+      default:
+        value += currentCharacter;
+    }
+  }
+
+  array.push(value);
+  if (isHeader) {
+    result.Header = array;
+  }
+  else {
+    result.Data.push(array);
+  }
+
   return result;
 }
